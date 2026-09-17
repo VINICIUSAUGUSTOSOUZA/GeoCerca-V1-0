1933dafd5823db0633659b258b7d258c3a6dd349
VINICIUSAUGUSTOSOUZA <personagem450@gmail.com>
2026-09-17 12:14:49 -0300

    Suportar identificação de múltiplos polígonos

--- app/src/main/java/com/geocerca/app/GeoPoint.java
+++ app/src/main/java/com/geocerca/app/GeoPoint.java
@@ -6,12 +6,18 @@ public class GeoPoint {
     public final double y;
     public final double z;
     public final boolean latLon;
+    public final int polygonId;
 
     public GeoPoint(String name, double x, double y, double z, boolean latLon) {
+        this(name, x, y, z, latLon, 0);
+    }
+
+    public GeoPoint(String name, double x, double y, double z, boolean latLon, int polygonId) {
         this.name = name;
         this.x = x;
         this.y = y;
         this.z = z;
         this.latLon = latLon;
+        this.polygonId = polygonId;
     }
 }
